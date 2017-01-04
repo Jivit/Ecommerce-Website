a202e1721d6de4c862478882ed1197e8d26df0c0
Vilas M <vilasnitk19@gmail.com>
2017-01-04 21:05:18 +0530

    Updated changes

--- Website/controllers/homecontroller.js
+++ Website/controllers/homecontroller.js
@@ -35,33 +35,57 @@ module.exports = function(app){
 		res.render('login.ejs');
 	});
 
-	var insertDocument = function(db,username,pwd, callback) {
-		   db.collection('users').insertOne( {
-		      "username" : username,
-		      "password" : pwd
-		   }, function(err, result) {
-		    assert.equal(err, null);
-		    console.log("Inserted a document into the users collection.");
-		    callback();
-  		});
-	};
-	var urlencodedParser = bodyParser.urlencoded({extended: false});
+		var insertDocument = function(db,username, pwd, mail, phno, callback) {
+			   db.collection('users').insertOne( {
+			      "username" : username,
+			      "password" : pwd,
+			      "mail" : mail,
+			      "phno" : phno
+			   }, function(err, result) {
+			    assert.equal(err, null);
+			    console.log("Inserted a document into the users collection.");
+			    callback();
+	  		});
+		};
+		var urlencodedParser = bodyParser.urlencoded({extended: false});
 
-	app.post('/userlogin', urlencodedParser, function(req, res) {
-    	var username  = req.body.username;
-    	var pwd = req.body.password;
-    	var url = "mongodb://localhost:27017/product_database";
-		MongoClient.connect(url, function(err, db) {
-		  assert.equal(null, err);
-		  insertDocument(db, username, pwd, function() {
-		      db.close();
-		  });
-		});
-        var html = 'Hello: ' + username + '.<br>' +'<a href="/">Try again.</a>';
-  		res.writeHead(301,{Location: '/'});
-		res.end();
+		app.post('/register', urlencodedParser, function(req, res) {
+	    	var username  = req.body.username;
+	    	var pwd = req.body.password;
+	    	var mail = req.body.mail;
+	    	var phno = req.body.phno;
+	    	var url = "mongodb://localhost:27017/product_database";
+			MongoClient.connect(url, function(err, db) {
+			  assert.equal(null, err);
+			  insertDocument(db, username, pwd, mail, phno, function() {
+			      db.close();
+			  });
+			});
+			// alert('Registered!! Please login with the details!');
+	  		res.writeHead(301,{Location: '/login'});
+			res.end();
 
-  	});
+	  	});
+	  	app.post('/loginres',urlencodedParser,function(req, res){
+	  		var username  = req.body.username;
+	    	var pwd = req.body.password;
+	    	var url = "mongodb://localhost:27017/product_database";
+	    	MongoClient.connect(url,function(err,db){
+						var cursor = db.collection('users').find();
+						var flag = 0;
+						cursor.each(function(err,doc){
+							if(doc != null){
+								console.log(doc.username);
+								if(doc.username == username && doc.password == pwd){
+									flag = 1;
+									res.send('<html><h1>LOG-IN WORKED</h1></html>');
+								}
+							}
 
+						});
+
+
+					});
+	  	});
 
 }
